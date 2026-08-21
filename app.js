bc4755ef8a7890922935df6a72cad8c871a3aad8
simonlavenne-bibli <simonlavenne@lalumiere.be>
2026-08-21 10:07:13 +0200

    Fix API_URL format in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // ====== CONFIGURATION ======
 // /!\ REMPLACEZ CETTE URL PAR CELLE FOURNIE PAR GOOGLE APPS SCRIPT LORS DU DÉPLOIEMENT /!\
-const API_URL = https://script.google.com/macros/s/AKfycbyouLRyvycutzzdjNtUuIlmOwZ3bqoNsQBlTiqn-FmKyPEa--vOlPecGIAdj-W1rRem;
+const API_URL = "https://script.google.com/macros/s/AKfycbyouLRyvycutzzdjNtUuIlmOwZ3bqoNsQBlTiqn-FmKyPEa--vOlPecGIAdj-W1rRem/exec";
+
 // ====== STATE ======
 let state = {
     user: { name: '', email: '' },
